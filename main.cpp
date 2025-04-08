28474a2582b58ab29b9711858d53a4ede255c212
HAHAH07 <a16227399100@163.com>
2025-04-08 09:20:34 +0800

    Update and rename test.c to main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fun.h"
+
+int main()
+{
+	string str1 = "hello fun";
+	cout << "hello main" << endl;
+	Fun_print(str1);
+	return 0;
+}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("hello test.c");
-  //从原本工程复制过来其他内容
-}
